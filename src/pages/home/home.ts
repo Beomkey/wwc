cbd7b630512fc3fea50bf39e12f6c4ee47fe8b96
shinjh0849 <21200421@handong.edu>
2018-06-03 16:03:13 +0900

    qr re

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -43,6 +43,6 @@ export class HomePage {
           // permission was denied, but not permanently. You can ask for permission again at a later time.
         }
       })
-      .catch((e: any) => console.log('Error is', e));
+      .catch((e: any) => alert('Error is ' + e));
   }
 }
