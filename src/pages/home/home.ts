146041963f6d5f7378b2f4f57f1635b2b2c25235
Beom Gayeon <21300356@handong.edu>
2018-06-06 12:56:33 +0900

    couponFromDb
    디비에 있는 쿠폰 읽어서 홈화면에 뿌려주기

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,24 +7,38 @@ import { QRscanPage } from '../q-rscan/q-rscan';
 
 import { AngularFireDatabase} from 'angularfire2/database';
 import firebase from 'firebase';
-
+import 'firebase/firestore';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  coups: Array<Object> = [];
 
   constructor(public navCtrl: NavController, public af: AngularFireDatabase, private barcode: BarcodeScanner) {
+    this.coups = [
+    ]
+
+    let db = firebase.firestore();
+    let uid = firebase.auth().currentUser.uid;
+
+    var coupsRef = db.collection(`Customer/${uid}/coupons`);
+    var coups = coupsRef.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        this.coups.push({cafe: doc.data().cafe, stemps: doc.data().stemps});
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
   }
-  goToCafeDetail(params) {
-    if (!params) params = {};
-    this.navCtrl.push(CafeDetailPage);
+  goCoupDetails(theCoup: any) {
+    this.navCtrl.push("CafeDetailPage", { coup: theCoup });
   }
 
-  ionViewDidLoad() {
-    console.log('home.ts loaded');
-  }
 
   onScanClick(){
     this.navCtrl.push(QRscanPage, {}, { animate: false });
