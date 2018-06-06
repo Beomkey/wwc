ca6154fd6471ba6e715e5ca26b5c6f4f26838fda
Beom Gayeon <21300356@handong.edu>
2018-06-06 14:55:19 +0900

    useCoupon

--- src/pages/cafe-detail/cafe-detail.ts
+++ src/pages/cafe-detail/cafe-detail.ts
@@ -1,16 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Coupon } from '../../models/coupon';
+import { HomePage } from '../home/home'
+
+import { AngularFireDatabase} from 'angularfire2/database';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   selector: 'page-cafe-detail',
   templateUrl: 'cafe-detail.html'
 })
 export class CafeDetailPage {
-  coupInfo: Object;
+  coupInfo = {} as Coupon;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.coupInfo = navParams.data.coup;
-
   }
-  
+  useCoupon() {
+    if (this.coupInfo.stemps<10) {
+      const alert = this.alertCtrl.create({
+        title: 'Cannot use!',
+        subTitle: 'You can use coupon collected at least 10 stemps',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    else {
+      let db = firebase.firestore();
+      let uid = firebase.auth().currentUser.uid;
+
+      var stempsRef = db.collection(`Customer/${uid}/coupons/`);
+      db.collection(`Customer/${uid}/coupons`).doc(`${this.coupInfo.cafe}`).set({
+        cafe: this.coupInfo.cafe,
+        stemps: this.coupInfo.stemps-10
+      }).then(function() {
+        console.log("Document successfully written!");
+      })
+      .catch(function(error) {
+        console.error("Error writing document: ", error);
+        return;
+       });
+       const alert = this.alertCtrl.create({
+        title: 'Used!',
+        subTitle: 'Coupon is used succesly',
+        buttons: ['OK']
+      });
+      alert.present();
+      this.navCtrl.setRoot(HomePage);  
+    }
+  }  
 }
